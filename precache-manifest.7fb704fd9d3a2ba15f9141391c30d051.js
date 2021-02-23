9293f08546c146aca32288d57c99b2980249e459
Roman Rangel <romanrangel106549@gmail.com>
2021-02-23 15:32:02 -0800

    Updates

---
+++ precache-manifest.7fb704fd9d3a2ba15f9141391c30d051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef08b362a0c83b50bc34",
+    "url": "/react-portfolio/static/css/main.97c71c15.chunk.css"
+  },
+  {
+    "revision": "ef08b362a0c83b50bc34",
+    "url": "/react-portfolio/static/js/main.ef08b362.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "e7ebf89e2e3f0ad1de6c71660b3044be",
+    "url": "/react-portfolio/index.html"
+  }
+];
